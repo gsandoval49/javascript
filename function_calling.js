7f9d258001c11c74ffcacaefadeccc390fd98a93
Giles Sandoval <gsandoval2277@gmail.com>
2020-06-11 23:35:43 -0600

    Create function_calling.js

---
+++ function_calling.js
@@ -0,0 +1,10 @@
+function sayThanks () {
+  console.log ('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+// call function 3x
+// you can reuse functions as many times as needed
