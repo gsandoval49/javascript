f32962f8726701762720954f0862a7986264cca9
Giles Sandoval <gsandoval2277@gmail.com>
2020-06-13 00:49:42 -0600

    Create default_parameters.js

---
+++ default_parameters.js
@@ -0,0 +1,7 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+// doesn't output anything but we assigned default parameter of milk, bread, and eggs as items we normally pick up at the grocery store. 
