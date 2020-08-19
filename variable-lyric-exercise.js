46245844043a37485431068fdd1db34b55d54512
Giles Sandoval <gsandoval2277@gmail.com>
2020-08-18 23:02:29 -0600

    Create variable-lyric-exercise.js

---
+++ variable-lyric-exercise.js
@@ -0,0 +1,9 @@
+// Declare a variable of your choice and assign it a string that's exactly the same as the variable name, 
+// but enclosed in quotation marks so JavaScript recognizes it as a string. 
+// Then code an alert, specifying the variable, not the string, as the message.
+
+var lyric; 
+lyric = "Keep on dreaming even if it breaks your heart";
+alert(lyric);
+
+// outputs Keep on dreaming even if it breaks your heart
