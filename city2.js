d8a8958fce813ec554773dbb2ae44f3b08fd6a23
Giles Sandoval <gsandoval2277@gmail.com>
2020-09-03 00:12:25 -0600

    Create city2.js

---
+++ city2.js
@@ -0,0 +1,7 @@
+
+const logCitySkyline = () => {
+  let city = 'New York City';
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+  console.log(logCitySkyline());
+} 
