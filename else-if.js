31db084855d30a2b2762959ff6e041abb3593aec
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-10-24 15:37:49 -0600

    intiial commit

---
+++ else-if.js
@@ -0,0 +1,9 @@
+const friendsAtYourParty = 3; 
+
+if (friendsAtYourParty === 0){
+    console.log("Cool, now i get all the nachos to myself");
+} else if (friendsAtYourParty >= 4) {
+    console.log("let's play Mario Kart");
+} else {
+    console.log("Wooo, lets have a dance party");
+}
