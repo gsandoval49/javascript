417cb13469f8ae686cec51dfb937036e1067eba7
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-10-24 15:18:29 -0600

    initial commit - variables learning

---
+++ experiments/experiments.js
@@ -0,0 +1,9 @@
+const monthlyRent = 500;
+
+let yearlyRent = monthlyRent * 12;
+console.log(monthlyRent, yearlyRent );
+console.log(yearlyRent);
+
+yearlyRent = 1;
+console.log(yearlyRent)
+console.log(monthlyRent)
