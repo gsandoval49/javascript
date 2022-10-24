21bf9651e5bb20df40a08bc30f139a754fcb9020
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-10-24 16:34:09 -0600

    initialcommit

---
+++ different-ways-incrementing-one.js
@@ -0,0 +1,13 @@
+let friendsAtYourParty = 0;
+
+while (friendsAtYourParty < 10) {
+    /* all of the lines below do the same thing. They add 1 */
+    friendsAtYourParty = friendsAtYourParty +1; 
+    friendsAtYourParty +=1; 
+    friendsAtYourParty ++;
+    ++friendsAtYourParty;
+    console.log(friendsAtYourParty); //this line will update the number until it's done
+
+}
+
+console.log(friendsAtYourParty);
