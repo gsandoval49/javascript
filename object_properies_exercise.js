1f6f9a246927eda74c65bbadcb0a14022cd894bb
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-12-21 22:57:49 -0700

    Initial commit

---
+++ object_properies_exercise.js
@@ -0,0 +1,12 @@
+var person = {
+	name: "John", age: 31, 
+	favColor: "green", height: 183
+};
+var x = person.age;
+var y = person['age'];
+const z = person.height;
+
+
+document.write(x);
+document.write(y);
+document.write(z);
