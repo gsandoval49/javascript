4c1b0ceee8050ea4b52e5c83e395035654944970
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2023-03-21 21:26:23 -0600

    intiial commit

---
+++ inner_html_js_buttons.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Seriously what can JS do? </h2>
+
+<p id="demo2">JavaScript (JS) can change HTML content.</p>
+<p id="demo3">This probably won't change </p>
+
+<button type="button" onclick='document.getElementById("demo2").innerHTML = "Hello JavaScript! WELCOME to my life."'>Click Me NOW!</button>
+
+</br>
+<button type="button"  onclick = 'document.getElementById("demo3").innerHTML = "Let us try this again"'>And this one</button>
+
+</body>
+</html>
