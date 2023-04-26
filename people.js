9d36eaffa93669f86b25f7374c9a65e8d2e02a12
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2023-04-25 21:56:17 -0600

    initial commit - practice

---
+++ people.js
@@ -0,0 +1,31 @@
+let friendsAtYourParty = 0;
+
+for (let i = 0; i < 12; i++) {
+    console.log(i);
+    friendsAtYourParty++;
+}
+
+console.log(friendsAtYourParty);
+
+let chosenDisciples = 0; 
+
+for (let a = 0; a < 13; a++) {
+    console.log("Disciple " + a);
+    chosenDisciples++;
+}
+
+console.log(chosenDisciples);
+
+let thesePeople = 0; 
+
+for (let b = 0; b <= 50; b++) {
+    console.log("Peeps " + b);
+    thesePeople++;
+}
+
+let somePeople = 0;
+
+for (let c = 0; c < 5; c++)  {
+    console.log("Gingerbread men " + c);
+    somePeople++;
+}
