c45f809235e8e7b043185a5943e90f6726e20644
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2024-10-29 13:04:07 -0600

    initial commit

---
+++ instantMethodsExamples.js
@@ -0,0 +1,8 @@
+// use the following in the console in your web tools
+// viewing the title of a webpage 
+
+// Get the current title of the document
+console.log(document.title); // Output: "Current Page Title"
+
+// Set a new title for the document
+document.title = "New Dynamic Title";
