fadf5dbdb1af6e69b9b590aac63e11ff3b14ef58
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2024-11-09 10:30:04 -0700

    added includes method example

--- console-operators.js
+++ console-operators.js
@@ -1,4 +1,9 @@
-// checks for certain case sensitive words in a title 
-// output is a boolean
-document.title.includes("Example Title Here")
+// ALL THESE EXAMPLES CAN BE USED IN WEB TOOLS CONSOLE
 
+// includes() checks for certain case sensitive words within a string to see if it exists
+// output is a boolean
+document.title.includes("Example Title Here");
+
+// tells you the content within a node 
+// outputs the string of content from the node from the DOM 
+document.querySelector("someNodeHere").textContent;
