af9b4633b80c8a937605f495bb47efc45d774eea
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2024-11-09 15:58:38 -0700

    typeOf operand explained

--- console-operators.js
+++ console-operators.js
@@ -7,3 +7,13 @@ document.title.includes("Example Title Here");
 // tells you the content within a node 
 // outputs the string of content from the node from the DOM 
 document.querySelector("someNodeHere").textContent;
+
+// typeOf operator returns what a data type is
+// for ex... boolean, number, string, etc. 
+console.log(typeof("yourVariableOrDataTypeHere"));
+  // this should output "string" 
+// or you can leave out "console.log" if you're in web tools and just place "typeOf"
+typeOf = 82;
+  // output >> "number"
+typeOf = true;
+  // output >> "boolean"
