3b2a7465ff3683cbd02f08d56cf6b9af8b78b586
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2024-11-30 13:31:11 -0700

    incorporate pop() function

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 let synonyms = ["plethora", "array", "cornucopia"]; // array pointing to synonyms 
 synonyms.length // gives me 3, number of syn in array
 synonyms[2] // returns cornucopia
+
+// pop() function will take the last item in the array and assign the value to lastItem
+let lastItem = synonyms.pop();
+synonyms.length // pop() also takes out last item, so now only 2 items in array
